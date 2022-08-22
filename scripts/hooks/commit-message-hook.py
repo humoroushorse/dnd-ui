48f9ec2d564dc76627dccee1185de29162caa850
Ian Kirkpatrick <iankirkpatrick@metalbackgorilla.com>
2022-08-21 23:07:50 -0600

    python git hook instead 2 DND-1: python git hook instead 2
    **JIRA**
    * [DND-1](https://ian-kirkpatrick.atlassian.net/browse/DND-1)
    
    **Description**
    python git hook instead 2

--- scripts/hooks/commit-message-hook.py
+++ scripts/hooks/commit-message-hook.py
@@ -9,15 +9,14 @@ ticket_prefix = re.findall("DND-[0-9]+", local_branch_name)[0]
 print(f'filepath: {commit_msg_filepath}')
 
 with open(commit_msg_filepath, 'r+') as f:
-  content = f.read()
-  print(f'content: {content}')
+  original_commit_message = f.read()
   if ticket_prefix:
     jira_snippet = f'\n**JIRA**\n* [{ticket_prefix}](https://ian-kirkpatrick.atlassian.net/browse/{ticket_prefix})'
 
-    if content.startswith(ticket_prefix):
-      content = content[len(ticket_prefix):]
+    if original_commit_message.startswith(ticket_prefix):
+      original_commit_message = original_commit_message[len(ticket_prefix):]
 
-    description = f'\n**Description**\n{content if content else "TODO: description"}'
+    description = f'\n**Description**\n{original_commit_message if original_commit_message else "TODO: description"}'
 
     print(f'{ticket_prefix}\n{jira_snippet}\n{description}\n')
-    f.write(f'{ticket_prefix}\n{jira_snippet}\n{description}\n')
+    f.write(f'{ticket_prefix}: {original_commit_message}\n{jira_snippet}\n{description}\n')
