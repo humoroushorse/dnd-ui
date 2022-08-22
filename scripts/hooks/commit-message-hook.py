9e54b4c4537ed281f6dcd8609f1efa3f65da0c51
Ian Kirkpatrick <iankirkpatrick@metalbackgorilla.com>
2022-08-21 23:16:53 -0600

    change commit message DND-1: change commit message  [DND-1](https://ian-kirkpatrick.atlassian.net/browse/DND-1)

--- scripts/hooks/commit-message-hook.py
+++ scripts/hooks/commit-message-hook.py
@@ -8,6 +8,7 @@ ticket_prefix = re.findall("DND-[0-9]+", local_branch_name)[0]
 
 with open(commit_msg_filepath, 'r+') as f:
   original_commit_message = f.read()
+  
   if ticket_prefix:
     if original_commit_message.startswith(ticket_prefix):
       original_commit_message = original_commit_message[len(ticket_prefix):]
