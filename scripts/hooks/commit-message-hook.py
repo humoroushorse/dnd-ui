1715a02e1ee0235b5db10c3c8b2b1f4f2545a7a8
Ian Kirkpatrick <iankirkpatrick@metalbackgorilla.com>
2022-08-21 23:21:43 -0600

    foobar DND-1: foobar  [DND-1](https://ian-kirkpatrick.atlassian.net/browse/DND-1)

--- scripts/hooks/commit-message-hook.py
+++ scripts/hooks/commit-message-hook.py
@@ -8,12 +8,12 @@ ticket_prefix = re.findall("DND-[0-9]+", local_branch_name)[0]
 
 with open(commit_msg_filepath, 'r+') as f:
   original_commit_message = f.read()
-  
+
   if ticket_prefix:
     if original_commit_message.startswith(ticket_prefix):
       original_commit_message = original_commit_message[len(ticket_prefix):]
 
     new_commit_message = f'{ticket_prefix}: {original_commit_message} [{ticket_prefix}](https://ian-kirkpatrick.atlassian.net/browse/{ticket_prefix})'
 
+    print(f'\n\n\t{new_commit_message}\n\n\n')
     f.write(new_commit_message)
-
